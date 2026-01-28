d676000a24a3708a7075e229e82f09b513de12c7
nkiyo <nakahara.kiyoshi@gmail.com>
2026-01-28 09:05:26 +0900

    update execute_api.sh

--- execute_api.sh
+++ execute_api.sh
@@ -3,4 +3,5 @@
 # curl -X POST http://localhost:3000/api/workflow/execute \
 #   -H "Content-Type: application/json" \
 #   -d '{ "num": 123 }'
-curl http://localhost:3000/api/workflow/execute
+# curl http://localhost:3000/api/workflow/execute
+curl https://main.d1kb6stlfij8ei.amplifyapp.com/api/workflow/execute
