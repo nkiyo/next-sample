f8f3550d9195d04a85b5503aea83c65a8be38be5
nkiyo <nakahara.kiyoshi@gmail.com>
2026-02-02 08:44:57 +0900

    フロントエンドにcognito認証UIを追加

---
+++ app/aws-exports.js
@@ -0,0 +1,8 @@
+const awsmobile = {
+    "aws_project_region": "us-east-1",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_1YTalfPTh",
+    "aws_user_pools_web_client_id": "3ris1jvmroe02so932g65o6eu3"
+}
+
+export default awsmobile;
